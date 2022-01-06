7c57701ca84d2f5a4f727489287ed54580cf8646
maynut <libes1234@naver.com>
2022-01-06 10:40:48 +0900

    ++

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Routes, Route, Link, useParams, useNavigate } from 'react-router-dom';
 
 import itemData from './testdata.json';
+import itemData2 from './itemdata.json';
 import './App.css';
 import React from 'react';
 
@@ -28,6 +29,30 @@ function Is2() {
   const [searchItem, setSearchItem] = useState(null);
   const [option, setOption] = useState('id');
 
+  let filter = itemData.filter((data) => {
+    if (searchItem == null || searchItem == "") {
+      return data;
+    }
+    else {
+      if (option == 'id') {
+        if (data.no.includes(searchItem)) { return data; }
+      } else if (option == 'name') {
+        if (data.name.toLowerCase().includes(searchItem.toLowerCase())) { return data; }
+      }
+    }
+  });
+
+  const items2 = Object.keys(itemData2);
+
+  let itemList = [];
+  let itemId = 1;
+
+  for (let i=0; i<Object.keys(itemData2).length; i++) {
+    itemList.push(
+      <Item2 data={Object.keys(itemData2)[i]} id={i} key={Object.keys(itemData2)[i]} />
+    );
+  }
+
   return(
     <>
       <header>
@@ -51,23 +76,16 @@ function Is2() {
       <main>
         <div className='is2__main'>
           <div className='is2-item__grid'>
-            {itemData.filter((data) => {
-              if (searchItem == null || searchItem == "") {
-                return data;
-              }
-              else {
-                if (option == 'id') {
-                  if (data.no.includes(searchItem)) { return data; }
-                } else if (option == 'name') {
-                  if (data.name.toLowerCase().includes(searchItem.toLowerCase())) { return data; }
-                }
-              }
-            }).map(item => (
-              <Item id={item.id} key={`item${item.id}`} />
+            {/* {filter.map(item => ( */}
+            {items2.map(item => (
+              // {/* <Item id={item.id} key={`item${item.id}`} /> */}
+              <Item2 data={item} id={itemId++} key={item} />
             ))}
+            {/* {itemList} */}
           </div>
           <Routes>
-            <Route path="/:itemId" element={<ItemDetail />} />
+            {/* <Route path="/:itemId" element={<ItemDetail />} /> */}
+            <Route path="/:itemId" element={<ItemDetail2 />} />
           </Routes>
         </div>
       </main>
@@ -89,6 +107,21 @@ function Item(props) {
   );
 }
 
+function Item2(props) {
+  const id = props.id;
+  const data = props.data;
+  const iconStyle = { backgroundImage: `url(${process.env.PUBLIC_URL+'/items/'+itemData2[data].id}.png)` }
+
+  return(
+    <div className='item' id={id}>
+      <div className='item-icon'
+        style={iconStyle}>
+        <Link to={`/is/season2/item/${id}`}><p>{id}</p></Link>
+      </div>
+    </div>
+  );
+}
+
 function ItemDetail() {
   const navigate = useNavigate();
   const id = useParams().itemId;
@@ -105,6 +138,29 @@ function ItemDetail() {
         </div>
         <div className='item-detail section__item'>
           {itemData[id].detail}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function ItemDetail2() {
+  const navigate = useNavigate();
+  const items2 = Object.keys(itemData2);
+  const id = useParams().itemId-1;
+  const iconStyle = { backgroundImage: `url(${process.env.PUBLIC_URL + '/items/' + itemData2[items2[id]].id}.png)` }
+
+  return(
+    <section className='item__section' id={id}>
+      <div className='item-section__background' onClick={() => { navigate("/is/season2/item"); }}></div>
+      <div className='item-section__inner'>
+        <div className='item-no section__item'>{id+1}</div>
+        <div className='item-icon section__item' style={iconStyle}></div>
+        <div className='item-name section__item'>
+          {itemData2[items2[id]].name}
+        </div>
+        <div className='item-detail section__item'>
+          {itemData2[items2[id]].usage}
         </div>
       </div>
     </section>
